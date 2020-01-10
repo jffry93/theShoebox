46b27d0e279865a12618264e1913d59d0ad95e9e
jffry93 <jay.zalischi@gmail.com>
2020-01-09 19:25:21 -0500

    added theShoeBox

---
+++ scriptabout.js
@@ -0,0 +1,20 @@
+/* back top button */
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 300) {
+            $('.my_bttn').fadeIn(250);
+        }
+        else {
+            $('.my_bttn').fadeOut(250);
+        }
+    });
+    $('.my_bttn').click(function () {
+        $('html,body').animate(
+            { scrollTop: 0 }, 400
+        );
+    })
+});
+
+/* ^^^^^^^^back to top button ^^^^^^^ */
+
