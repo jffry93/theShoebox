41a8332fd75f66580f43d33a828bb670605570b7
jffry93 <jay.zalischi@gmail.com>
2020-01-19 13:11:22 -0500

    random number generator
    created the strings in js so the random number populating would not change the layout of the page.

--- originyear.js
+++ originyear.js
@@ -1,3 +1,5 @@
+//Random number generator
+
 let upper = 2020;
 let lower = 1993;
 
@@ -5,4 +7,4 @@ let random = Math.floor(Math.random() * (upper - lower + 1) + lower)
 
 console.log(random);
 
-document.getElementById('originyear').innerHTML = random;
+document.getElementById('originyear').innerHTML = "The idea for TheShoeBox was born in " + random + "  when founder, Jeff Zalischi, wanted nice shoes but didn't have the time to shop for them. Being someone that is very active and on the go, he would contantly have to replace his shoes every couple months. ";
